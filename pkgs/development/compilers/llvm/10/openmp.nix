c3360fb589b71e1985aab0396b8828aba4185789
Gabor Greif <ggreif@gmail.com>
2020-01-25 00:17:00 +0100

    llvmPackages_10: copy llvmPackages_9
    * make `lldb` compilable again on Darwin
    * minor tweaks to the patching of sources,
      some of which are backportable to earlier versions

---
+++ pkgs/development/compilers/llvm/10/openmp.nix
@@ -0,0 +1,26 @@
+{ stdenv
+, fetch
+, cmake
+, llvm
+, perl
+, version
+}:
+
+stdenv.mkDerivation rec {
+  pname = "openmp";
+  inherit version;
+
+  src = fetch pname "0yd96qxy8a6bvdiy1jjfjybsx87scha34szkvv1h2q7zhf8dj0ag";
+
+  nativeBuildInputs = [ cmake perl ];
+  buildInputs = [ llvm ];
+
+  enableParallelBuilding = true;
+
+  meta = {
+    description = "Components required to build an executable OpenMP program";
+    homepage    = http://openmp.llvm.org/;
+    license     = stdenv.lib.licenses.mit;
+    platforms   = stdenv.lib.platforms.all;
+  };
+}
