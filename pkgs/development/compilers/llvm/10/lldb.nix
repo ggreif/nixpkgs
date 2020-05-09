c71d0b5ad23f184540fb7fdc3da307163625feaa
Holger Wünsche <holger.o.wuensche@t-online.de>
2020-05-09 19:59:12 +0200

    lldb_10: explicitly fixed python path
    lldb might pick wrong python during installation. This chage explicitly
    tells lldb which path to use. LLDB_RELOCATABLE_PYTHON set to its default
    value.

--- pkgs/development/compilers/llvm/10/lldb.nix
+++ pkgs/development/compilers/llvm/10/lldb.nix
@@ -145,6 +145,8 @@ stdenv.mkDerivation (rec {
     "-DLLVM_ENABLE_RTTI=OFF"
     "-DClang_DIR=${clang-unwrapped}/lib/cmake"
     "-DLLVM_EXTERNAL_LIT=${lit}/bin/lit"
+    "-DPYTHON_HOME=${python3}"
+    "-DLLDB_RELOCATABLE_PYTHON=0"
   ] ++ stdenv.lib.optionals stdenv.isDarwin [
     "-DLLDB_USE_SYSTEM_DEBUGSERVER=ON"
   ] ++ stdenv.lib.optionals (!stdenv.isDarwin) [
